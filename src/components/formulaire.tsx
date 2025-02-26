c21c2b50b23882773a7fdaabe8e1ae441e30826e
SoanavalonaDeborah <deborahraza@protonmail.com>
2025-02-26 23:09:29 +0300

    furmulaire component

--- src/components/formulaire.tsx
+++ src/components/formulaire.tsx
@@ -1,7 +1,39 @@
+import { useState } from "react"
 
 
 export default function Formulaire() {
+    const [element, setElement] = useState<string[]>([]);
+    const [newElement, setNewElement] = useState<string>("");
+
+    const addElement = () => {
+        setElement([...element, newElement]);
+        setNewElement("");
+    }
+
+    const deleteElement = (index: number) => {
+        const updatedElement = [...element];
+        updatedElement.splice(index, 1);
+        setElement(updatedElement);
+    }
+
     return (
-        <p>Hello World !</p>
+        <div>
+      <h1>Liste Dynamique</h1>
+      <ul>
+        {element.map((element, index) => (
+          <li key={index}>
+            {element}
+            <button onClick={() => deleteElement(index)}>Supprimer</button>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newElement}
+        onChange={(e) => setNewElement(e.target.value)}
+        placeholder="Ajouter un élément"
+      />
+      <button onClick={addElement}>Ajouter</button>
+    </div>
     )
 }
